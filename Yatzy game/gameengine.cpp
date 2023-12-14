a201048dfe3d2872fadb47f53b4e8b5528a27e73
JustusHaataja <124276677+JustusHaataja@users.noreply.github.com>
2023-12-14 15:06:17 +0200

    gameengine.cpp

--- Yatzy game/gameengine.cpp
+++ Yatzy game/gameengine.cpp
@@ -1,13 +1,3 @@
-/*
- * Course: COMP.CS.110 Programming 2: structures
- * Project 4: Yatzy
- * Name: Justus Haataja
- * Student ID: H291931
- * Username: xfjuha
- * E-mail: justus.haataja@tuni.fi
- *
- */
-
 #include "gameengine.hh"
 #include "functions.hh"
 #include "mainwindow.hh"
