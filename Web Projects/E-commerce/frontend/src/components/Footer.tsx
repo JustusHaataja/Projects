a3d025741787333f5da2bc999ecff4b81949a3cc
Justus Haataja <justus.haataja@hotmail.com>
2025-11-12 13:26:15 +0200

    feat: update Footer component with social media links and icons

--- Web Projects/E-commerce/frontend/src/components/Footer.tsx
+++ Web Projects/E-commerce/frontend/src/components/Footer.tsx
@@ -1,11 +1,29 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faTiktok } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
         <footer style={{ padding: "1rem", backgroundColor: "#ee", marginTop: "2rem" }}>
-            <p>© 2025 My E-Commerce Store</p>
+            <Link to="https://www.puhdistamo.fi/"
+                style={{ textDecoration: "none", color: "inherit" }}>Puhdistamo</Link>
+
+            <Link to="https://www.instagram.com/puhdistamo/"
+                style={{ textDecoration: "none", color: "inherit" }}>
+                    <FontAwesomeIcon icon={faInstagram} />
+                </Link>
+
+            <Link to="https://www.tiktok.com/@puhdistamoofficial"
+                style={{ textDecoration: "none", color: "inherit" }}>
+                    <FontAwesomeIcon icon={faTiktok} />
+                </Link>
+            
             <Link to="https://github.com/JustusHaataja/Projects"
-                style={{ textDecoration: "none", color: "inherit" }}>GitHub</Link>
+                style={{ textDecoration: "none", color: "inherit" }}>
+                    <FontAwesomeIcon icon={faGithub} />
+                </Link>
+
+            <p>© 2025 My E-Commerce Store</p>
         </footer>
     )
 }
