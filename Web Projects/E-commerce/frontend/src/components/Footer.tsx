1e503acaa1f7de04496a240f499c40312ba5ad60
Justus Haataja <justus.haataja@hotmail.com>
2025-11-13 15:29:54 +0200

    feat: update Footer component with social media links, payment icons, and improved layout

--- Web Projects/E-commerce/frontend/src/components/Footer.tsx
+++ Web Projects/E-commerce/frontend/src/components/Footer.tsx
@@ -1,52 +1,99 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faTiktok, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faTiktok, faGithub, faApplePay, faGooglePay, faPaypal} from '@fortawesome/free-brands-svg-icons';
+import { faPiggyBank } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
         <footer 
-            style={{ 
-                padding: "8px",
+            style={{
+                height:"250px",
+                width:"100%",
                 backgroundColor: "#eee",
                 display: "grid",
-                gridTemplateColumns: "1fr 1fr 1fr"
+                gridTemplateRows: "150px 100px",
+                textAlign: "center"
             }}>
-            
-            <ul
+            <div
                 style={{
-                    listStyle: "none",
-                    display: "flex",
-                    gap: "16px",
-                    margin: ""
+                    display: "grid",
+                    gridTemplateColumns: "1fr 1fr 1fr"
                 }}
-            >Seuraa meitä 
-                <li>
-                    <Link to="https://www.puhdistamo.fi/"
-                        style={{ textDecoration: "none", color: "inherit", fontSize: "24px"}}>
-                            Puhdistamo.fi
-                        </Link>
-                </li>
-                <li>
-                    <Link to="https://www.instagram.com/puhdistamo/"
-                        style={{ textDecoration: "none", color: "inherit" }}>
-                            <FontAwesomeIcon icon={faInstagram} style={{fontSize: "32px"}}/>
-                        </Link>
-                </li>
-                <li>
-                    <Link to="https://www.tiktok.com/@puhdistamoofficial"
-                        style={{ textDecoration: "none", color: "inherit" }}>
-                            <FontAwesomeIcon icon={faTiktok} style={{fontSize: "32px"}}/>
-                        </Link>
-                </li>
-                <li>
-                    <Link to="https://github.com/JustusHaataja/Projects"
-                        style={{ textDecoration: "none", color: "inherit" }}>
-                            <FontAwesomeIcon icon={faGithub} style={{fontSize: "32px"}}/>
-                        </Link>
-                </li>
-            </ul>
-            <p>Koira</p>
-            <p>© 2025 My E-Commerce Store</p>
+            >
+                <ul
+                    style={{
+                        listStyle: "none",
+                        display: "flex",
+                        gap: "16px",
+                        margin: ""
+                    }}
+                >Seuraa meitä 
+                    <li>
+                        <Link to="https://www.puhdistamo.fi/"
+                            style={{ textDecoration: "none", color: "inherit", fontSize: "24px"}}>
+                                Puhdistamo.fi
+                            </Link>
+                    </li>
+                    <li>
+                        <Link to="https://www.instagram.com/puhdistamo/"
+                            style={{ textDecoration: "none", color: "inherit" }}>
+                                <FontAwesomeIcon icon={faInstagram} style={{fontSize: "32px"}}/>
+                            </Link>
+                    </li>
+                    <li>
+                        <Link to="https://www.tiktok.com/@puhdistamoofficial"
+                            style={{ textDecoration: "none", color: "inherit" }}>
+                                <FontAwesomeIcon icon={faTiktok} style={{fontSize: "32px"}}/>
+                            </Link>
+                    </li>
+                    <li>
+                        <Link to="https://github.com/JustusHaataja/Projects"
+                            style={{ textDecoration: "none", color: "inherit" }}>
+                                <FontAwesomeIcon icon={faGithub} style={{fontSize: "32px"}}/>
+                            </Link>
+                    </li>
+                </ul>
+                <div>
+                    <p>Koira</p>
+                </div>
+                <div>
+                    <p>© {new Date().getFullYear()} My E-Commerce Store</p>
+                </div>    
+            </div>
+            <div
+                style={{
+                    background: "#555",
+                    color: "white",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "flex-start",
+                    padding: "16px"
+                }}
+            >
+                <p
+                    style={{
+                        margin: "0",
+                        marginBottom: "8px",
+                        fontWeight: "600"
+                    }}
+                >
+                    Maksutavat
+                </p>
+                <div
+                    style={{
+                        display: "flex",
+                        gap: "16px",
+                        alignItems: "center",
+                        // paddingTop: "8px"
+                    }}
+                >
+                    <FontAwesomeIcon icon={faApplePay} style={{ fontSize: "40px" }}/>
+                    <FontAwesomeIcon icon={faGooglePay} style={{ fontSize: "40px" }}/>
+                    <FontAwesomeIcon icon={faPaypal} style={{ fontSize: "24px" }}/>
+                    <FontAwesomeIcon icon={faPiggyBank} style={{ fontSize: "24px" }}/>
+                </div>
+            </div>
         </footer>
     )
 }
