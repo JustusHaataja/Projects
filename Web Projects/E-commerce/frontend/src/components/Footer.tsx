4a31d6c896e3ef34c0abdab7ec8e015b0c5ff8d9
Justus Haataja <justus.haataja@hotmail.com>
2025-11-13 16:21:56 +0200

    feat: update Footer component with improved layout, social media links, and dynamic copyright year

--- Web Projects/E-commerce/frontend/src/components/Footer.tsx
+++ Web Projects/E-commerce/frontend/src/components/Footer.tsx
@@ -7,59 +7,57 @@ const Footer = () => {
     return (
         <footer 
             style={{
-                height:"250px",
+                height:"300px",
                 width:"100%",
                 backgroundColor: "#eee",
                 display: "grid",
-                gridTemplateRows: "150px 100px",
+                gridTemplateRows: "auto 100px",
                 textAlign: "center"
             }}>
             <div
                 style={{
                     display: "grid",
                     gridTemplateColumns: "1fr 1fr 1fr"
-                }}
-            >
-                <ul
-                    style={{
-                        listStyle: "none",
-                        display: "flex",
-                        gap: "16px",
-                        margin: ""
-                    }}
-                >Seuraa meitä 
-                    <li>
-                        <Link to="https://www.puhdistamo.fi/"
-                            style={{ textDecoration: "none", color: "inherit", fontSize: "24px"}}>
-                                Puhdistamo.fi
-                            </Link>
-                    </li>
-                    <li>
-                        <Link to="https://www.instagram.com/puhdistamo/"
-                            style={{ textDecoration: "none", color: "inherit" }}>
-                                <FontAwesomeIcon icon={faInstagram} style={{fontSize: "32px"}}/>
-                            </Link>
-                    </li>
-                    <li>
-                        <Link to="https://www.tiktok.com/@puhdistamoofficial"
-                            style={{ textDecoration: "none", color: "inherit" }}>
-                                <FontAwesomeIcon icon={faTiktok} style={{fontSize: "32px"}}/>
-                            </Link>
-                    </li>
-                    <li>
-                        <Link to="https://github.com/JustusHaataja/Projects"
-                            style={{ textDecoration: "none", color: "inherit" }}>
-                                <FontAwesomeIcon icon={faGithub} style={{fontSize: "32px"}}/>
-                            </Link>
-                    </li>
-                </ul>
+                }}>
                 <div>
                     <p>Koira</p>
                 </div>
+
                 <div>
                     <p>© {new Date().getFullYear()} My E-Commerce Store</p>
-                </div>    
+                </div>
+
+                <div
+                    style={{
+
+                    }}>
+                    <Link to="https://www.puhdistamo.fi/"
+                        style={{
+                            textDecoration: "none",
+                            color: "inherit",
+                            fontSize: "24px",
+                            fontWeight: "600"
+                        }}>
+                            Puhdistamo.fi
+                    </Link>
+                    <div>
+                        <Link to="https://www.instagram.com/puhdistamo/"
+                            style={{ textDecoration: "none", color: "inherit" }}>
+                                <FontAwesomeIcon icon={faInstagram} style={{fontSize: "32px"}}/>
+                        </Link>
+                        <Link to="https://www.tiktok.com/@puhdistamoofficial"
+                            style={{ textDecoration: "none", color: "inherit" }}>
+                                <FontAwesomeIcon icon={faTiktok} style={{fontSize: "32px"}}/>
+                        </Link>
+                        <Link to="https://github.com/JustusHaataja/Projects"
+                            style={{ textDecoration: "none", color: "inherit" }}>
+                                <FontAwesomeIcon icon={faGithub} style={{fontSize: "32px"}}/>
+                        </Link>
+                    </div>
+                </div>
+  
             </div>
+
             <div
                 style={{
                     background: "#555",
