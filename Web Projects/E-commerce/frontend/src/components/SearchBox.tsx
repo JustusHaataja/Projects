56bfa149709c5559871bef2f84841c1765b5648e
Justus Haataja <justus.haataja@hotmail.com>
2025-11-24 17:41:45 +0200

    feat: implement SearchBox component with styling and layout adjustments

--- Web Projects/E-commerce/frontend/src/components/SearchBox.tsx
+++ Web Projects/E-commerce/frontend/src/components/SearchBox.tsx
@@ -1,3 +1,4 @@
+import '../styles/SearchBox.css';
 import React, { type ChangeEvent } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
@@ -10,37 +11,17 @@ interface SearchBoxProps {
 
 const SearchBox: React.FC<SearchBoxProps> = ({ value, onChange }) => {
     return (
-        <div
-            style={{
-                position: "relative",
-                display: "inline-block"
-            }}
-        >
+        <div className="searchbox-container" >
             <FontAwesomeIcon 
+                className="search-icon"
                 icon={faMagnifyingGlass}
-                style={{
-                    height: "16px",
-                    position: "absolute",
-                    left: "8px",
-                    top: "50%",
-                    transform: "translateY(-50%)",
-                    color: "#555",
-                    pointerEvents: "none"
-                }} 
             />
-            <input
+            <input className="search-input-box"
                 type="text"
                 placeholder="Hae tuotteita..."
                 value={value}
                 onChange={onChange}
                 aria-label="Search products"
-                style={{
-                    paddingLeft: "32px",
-                    height: "24px",
-                    borderRadius: "8px",
-                    border: "1px solid #ccc",
-                    width: "12rem"
-                }}
             />
         </div>
     )
