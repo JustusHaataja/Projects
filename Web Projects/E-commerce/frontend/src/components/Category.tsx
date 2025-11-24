d3e2c29296e910b8269b7a36837f3cafe93affbf
Justus Haataja <justus.haataja@hotmail.com>
2025-11-24 12:49:08 +0200

    chore(types): add src/global.d.ts for asset & Swiper CSS declarations

--- Web Projects/E-commerce/frontend/src/components/Category.tsx
+++ Web Projects/E-commerce/frontend/src/components/Category.tsx
@@ -2,13 +2,31 @@ import { useEffect, useState } from 'react';
 import { fetchCategories, type Category } from '../api/categories';
 import CategorySkeleton from './CategorySkeleton';
 
+import electrolytes from '../assets/electrolytes3.jpg';
+import kombucha from '../assets/kombucha3.jpg';
+import energydrink from '../assets/pineapple.jpg';
+import protein from '../assets/protein.jpg';
+
+
+const CATEGORY_IMAGES: Record<number, string> = {
+    1: energydrink,
+    2: electrolytes,
+    3: kombucha,
+    4: protein
+}
+
+const getGategoryImage = (id: number) => {
+    return CATEGORY_IMAGES[id];
+}
+
+
 const Categories = () => {
     const [categories, setCategories] = useState<Category[] | null>(null);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         // AbortController allows us to cancel the API request if the component
-        //  is removed from the DOM (no longer needed)
+        // is removed from the DOM (no longer needed)
         const controller = new AbortController();
 
         setError(null);
@@ -37,13 +55,25 @@ const Categories = () => {
 
             <div
                 style={{
-                    display: "flex"
+                    display: "flex",
+                    gap: "24px"
                 }}
             >
                 {categories?.map((cat) => (
                     <div
                         key={cat.id}
+                        style={{ display: "grid", justifyItems: "center"}}
                     >
+                        <img 
+                            src={getGategoryImage(cat.id)}
+                            alt={cat.name}
+                            style={{
+                                width: "120px",
+                                height: "120px",
+                                objectFit: "cover",
+                                borderRadius: "50%"
+                            }}
+                        />
                         {cat.name}
                     </div>
                 ))}
