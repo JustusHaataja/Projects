4c224ba0942ba7107a399d351dddeef537190ef4
Justus Haataja <justus.haataja@hotmail.com>
2025-11-26 15:16:41 +0200

    style: add AboutUs component with animated header and background image

--- Web Projects/E-commerce/frontend/src/components/AboutUs.tsx
+++ Web Projects/E-commerce/frontend/src/components/AboutUs.tsx
@@ -1,7 +1,24 @@
+import '../styles/AboutUs.css';
+
 const AboutUs = () => {
     return (
-        <div>
-            
+        <div className="about-container" >
+            <div className="rolling-header" >
+                <span className="letter" >P</span>
+                <span className="letter" >u</span>
+                <span className="letter" >h</span>
+                <span className="letter" >d</span>
+                <span className="letter" >i</span>
+                <span className="letter" >s</span>
+                <span className="letter" >t</span>
+                <span className="letter" >a</span>
+                <span className="letter" >m</span>
+                <span className="letter" >o</span>
+            </div>
+
+            <div className="" >
+
+            </div>
         </div>
     )
 }
