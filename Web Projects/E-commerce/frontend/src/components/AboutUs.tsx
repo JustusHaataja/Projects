461b2d2283d41253b2b4fa3a8c2ba1b6fe461750
Justus Haataja <justus.haataja@hotmail.com>
2025-12-01 14:50:11 +0200

    style: enhance AboutUs component layout and styling, add new end section

--- Web Projects/E-commerce/frontend/src/components/AboutUs.tsx
+++ Web Projects/E-commerce/frontend/src/components/AboutUs.tsx
@@ -5,7 +5,6 @@ import { faGem } from '@fortawesome/free-regular-svg-icons';
 import { faSeedling, faDumbbell } from '@fortawesome/free-solid-svg-icons';
 
 import bg1 from '../assets/energydrink2.jpg';
-import bg2 from '../assets/watermelon.jpg';
 import ed_img from '../assets/energydrinks.jpg';
 import superfood_img from '../assets/superfood.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -41,49 +40,46 @@ const AboutUs = () => {
                     </span>
                 ))}
             </div>
+            
+            <div className="about-hero" >
+                <img src={bg1}
+                    className="bg1-image"
+                    alt="Background image of a energydrink can"
+                />
 
-            <img src={bg1}
-                className="bg1-image"
-                alt="Background image of a energydrink can"
-            />
-
-            <div className="quality-container" >
-                <h1>
-                    Laatulupaus
-                </h1>
-                <p>
-                    Perustamisestamme lähtien meitä on ohjannut järkkymätön tahto 
-                    tarjota asiakkaillemme vain ja ainoastaan laadukkaimpia 
-                    mahdollisia tuotteita. Kun tiedät verkkokaupasta tilaamasi 
-                    tai kaupan hyllyltä poimimasi tuotteen noudattavan aina 
-                    Puhdistamon tiukkaa laatureseptiä, tulee hyvien valintojen 
-                    tekemisestä helpompaa.
-                </p>
+                <div className="quality-container" >
+                    <h1 className="quality-header" >
+                        Laatulupaus
+                    </h1>
+                    <p>
+                        Perustamisestamme lähtien meitä on ohjannut järkkymätön tahto 
+                        tarjota asiakkaillemme vain ja ainoastaan laadukkaimpia 
+                        mahdollisia tuotteita. Kun tiedät verkkokaupasta tilaamasi 
+                        tai kaupan hyllyltä poimimasi tuotteen noudattavan aina 
+                        Puhdistamon tiukkaa laatureseptiä, tulee hyvien valintojen 
+                        tekemisestä helpompaa.
+                    </p>
+                </div>
             </div>
-
-            <img src={bg2}
-                className="bg2-image"
-                alt="Background image of a energydrink can"
-            />
 
             <div className="value-container" >
                 <div className="value-box" >
                     <FontAwesomeIcon icon={faGem} className="value-icon" />
-                    <h1>Vain olennainen</h1>
+                    <h1 className="value-header" >Vain olennainen</h1>
                     <p>
                         Karsimme pois kaiken tarpeettoman ja epäterveellisen.
                     </p>
                 </div>
                 <div className="value-box" >
                     <FontAwesomeIcon icon={faSeedling} className="value-icon" />
-                    <h1>Puhtaus</h1>
+                    <h1 className="value-header" >Puhtaus</h1>
                     <p>
-                        Käytämme tuotteissamme vain parhaita mahdollisia raaka-aineita.
+                        Käytämme tuotteissamme vain parhaita raaka-aineita.
                     </p>
                 </div>
                 <div className="value-box" >
                     <FontAwesomeIcon icon={faDumbbell} className="value-icon" />
-                    <h1>Vaikutus</h1>
+                    <h1 className="value-header" >Vaikutus</h1>
                     <p>
                         Keskitymme tuotteidemme toimivuuteen ja tehokkuuteen.
                     </p>
@@ -162,6 +158,30 @@ const AboutUs = () => {
                         Kaikki superfoodimme sisältävät runsaan määrän ravinteita tiiviissä paketissa.
                     </p>
                 </div>
+            </div>
+
+            <div className="end-container" >
+                <h1 className="end-header" >Laatu syntyy siellä, missä osaaminen on vahvinta</h1>
+                <p>
+                    Kehitämme valtaosan tuotteistamme itse, ja suurin osa 
+                    ravintolisätuotteistamme valmistetaan omalla tehtaallamme Lempäälässä.
+                </p>
+                <p>
+                    Hankimme raaka-aineet aina sieltä, mistä löydämme parhaan 
+                    mahdollisen laadun. Suurin osa vitamiinien ja mineraalien 
+                    maailmanlaajuisesta tuotannosta sijaitsee Suomen ulkopuolella, 
+                    minkä vuoksi raaka-aineemme tulevat pääosin kansainvälisiltä, 
+                    tarkoin valituilta ja luotettavilta toimittajilta. 
+                    Esimerkiksi Super D-vitamiinimme tulee Ranskasta, glutamiinimme 
+                    tulee Etelä-Koreasta ja pakurimme Suomesta. Ruoka- ja 
+                    juomatuotteistamme esimerkiksi oliiviöljy valmistetaan 
+                    Kreikassa ja energiajuoma Ruotsissa.
+                </p>
+                <p>
+                    Meillä on kansainvälisesti arvostettu FSSC 22000 
+                    -elintarviketurvallisuussertifikaatti, ja kaikki tuotteemme 
+                    valmistetaan EU-lainsäädännön vaatimusten mukaisesti.
+                </p>
             </div>
 
         </div>
